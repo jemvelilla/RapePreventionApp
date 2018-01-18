19e028da5a37cd75d8f38e11985b9357e5401e0d
jemvelilla <jemvelilla@gmail.com>
2018-01-19 00:18:40 +0800

    Login Authentication added.

---
+++ app/src/main/java/com/build1/rapepreventionapp/Home.java
@@ -0,0 +1,29 @@
+package com.build1.rapepreventionapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Home extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+
+    public void btnOnClickLogout(View v){
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signOut();
+        startActivity(new Intent(getApplicationContext(), Login.class));
+    }
+
+}
--- app/src/main/java/com/build1/rapepreventionapp/RegisterStep4.java
+++ app/src/main/java/com/build1/rapepreventionapp/RegisterStep4.java
@@ -2,11 +2,18 @@ package com.build1.rapepreventionapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.Slide;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -16,32 +23,37 @@ public class RegisterStep4 extends AppCompatActivity {
     FirebaseDatabase database;
     DatabaseReference user;
 
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_step4);
 
         database = FirebaseDatabase.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
 
     }
     public void btnOnClickNextPassword(View v){
+
         EditText password = (EditText) findViewById(R.id.editTextPassword);
         EditText confPassword = (EditText) findViewById(R.id.editTextConfPassword);
 
         info = (UserInformation) getIntent().getSerializableExtra("info");
 
-        if (password.getText().toString().equals(confPassword.getText().toString())){
+        String email = info.getEmail();
+        String pass = password.getText().toString().trim();
 
-            Intent i = new Intent(getApplicationContext(), AccountCreated.class);
-            startActivity(i);
+        if (password.getText().toString().equals(confPassword.getText().toString())){
 
             user = database.getReference("Users").child(info.getFirstName() + " " + info.getLastName());
 
-            user.child("email").setValue(info.getEmail());
+            user.child("email").setValue(email);
             user.child("mobile_number").setValue(info.getMobileNumber().toString());
             user.child("first_name").setValue(info.getFirstName().toString());
             user.child("last_name").setValue(info.getLastName().toString());
             user.child("age").setValue(Integer.toString(info.getAge()));
+            user.child("birthdate").setValue(info.getBirthday().toString());
             user.child("current_address").setValue(info.getCurrentAddress().toString());
             user.child("contact_person1").setValue(info.getContactPerson1().toString());
             user.child("contact_number_person1").setValue(info.getContactNumber1().toString());
@@ -52,6 +64,19 @@ public class RegisterStep4 extends AppCompatActivity {
                 user.child("contact_person3").setValue(info.getContactPerson3().toString());
                 user.child("contact_number_person3").setValue(info.getContactNumber3().toString());
             }
+
+            firebaseAuth.createUserWithEmailAndPassword(email,pass)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Intent i = new Intent(getApplicationContext(), AccountCreated.class);
+                                startActivity(i);
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Registration failed. Please try again.", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
 
         }else{
 
