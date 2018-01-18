b4a27acc84b50e6da42bbb950951aa7b4f59b180
jemvelilla <jemvelilla@gmail.com>
2018-01-19 00:18:40 +0800

    Login Authentication added.

--- app/src/main/java/com/build1/rapepreventionapp/Welcome.java
+++ app/src/main/java/com/build1/rapepreventionapp/Welcome.java
@@ -2,16 +2,46 @@ package com.build1.rapepreventionapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Welcome extends AppCompatActivity {
 
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser()!=null){
+                    Intent intent = new Intent(getApplicationContext(), Home.class);
+                    startActivity(intent);
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v("message","onStart");
+        mAuth.addAuthStateListener(authStateListener);
     }
 
     public void btnOnClickSignUp(View v){
