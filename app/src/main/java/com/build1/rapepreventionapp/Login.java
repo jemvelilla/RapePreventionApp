558b108466c22056506731d34185b4b2be117223
jemvelilla <jemvelilla@gmail.com>
2018-01-19 22:55:09 +0800

    finish for logout

--- app/src/main/java/com/build1/rapepreventionapp/Login.java
+++ app/src/main/java/com/build1/rapepreventionapp/Login.java
@@ -44,6 +44,7 @@ public class Login extends AppCompatActivity {
                 if (firebaseAuth.getCurrentUser()!=null){
                     Intent intent = new Intent(getApplicationContext(), Home.class);
                     startActivity(intent);
+                    finish();
                 }
             }
         };
