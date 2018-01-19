839458f8819a876cbd4a5f8f3de705a77b3ca07d
unknown <darisevillareal@gmail.com>
2018-01-20 04:14:29 +0800

    password pattern, map fragment act button sign out

--- app/src/main/java/com/build1/rapepreventionapp/RegisterStep4.java
+++ app/src/main/java/com/build1/rapepreventionapp/RegisterStep4.java
@@ -43,45 +43,53 @@ public class RegisterStep4 extends AppCompatActivity {
 
         String email = info.getEmail();
         String pass = password.getText().toString().trim();
+        String confpass = confPassword.getText().toString().trim();
+        String pattern = "((?=.*\\d)(?=.*[A-Z]).{6,20})";
 
-        if (password.getText().toString().equals(confPassword.getText().toString())){
 
-            user = database.getReference("Users").child(info.getFirstName() + " " + info.getLastName());
 
-            user.child("email").setValue(email);
-            user.child("mobile_number").setValue(info.getMobileNumber().toString());
-            user.child("first_name").setValue(info.getFirstName().toString());
-            user.child("last_name").setValue(info.getLastName().toString());
-            user.child("age").setValue(Integer.toString(info.getAge()));
-            user.child("birthdate").setValue(info.getBirthday().toString());
-            user.child("current_address").setValue(info.getCurrentAddress().toString());
-            user.child("contact_person1").setValue(info.getContactPerson1().toString());
-            user.child("contact_number_person1").setValue(info.getContactNumber1().toString());
-            user.child("contact_person2").setValue(info.getContactPerson2().toString());
-            user.child("contact_number_person2").setValue(info.getContactNumber2().toString());
+        if (pass.matches(pattern) && confpass.matches(pattern)){
+            if (password.getText().toString().equals(confPassword.getText().toString())){
 
-            if(info.getContactPerson3() != null && info.getContactNumber3()!= null) {
-                user.child("contact_person3").setValue(info.getContactPerson3().toString());
-                user.child("contact_number_person3").setValue(info.getContactNumber3().toString());
-            }
+                user = database.getReference("Users").child(info.getFirstName() + " " + info.getLastName());
 
-            firebaseAuth.createUserWithEmailAndPassword(email,pass)
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()){
-                                Intent i = new Intent(getApplicationContext(), AccountCreated.class);
-                                startActivity(i);
-                            } else {
-                                Toast.makeText(getApplicationContext(), "Registration failed. Please try again.", Toast.LENGTH_LONG).show();
+                user.child("email").setValue(email);
+                user.child("mobile_number").setValue(info.getMobileNumber().toString());
+                user.child("first_name").setValue(info.getFirstName().toString());
+                user.child("last_name").setValue(info.getLastName().toString());
+                user.child("age").setValue(Integer.toString(info.getAge()));
+                user.child("birthdate").setValue(info.getBirthday().toString());
+                user.child("current_address").setValue(info.getCurrentAddress().toString());
+                user.child("contact_person1").setValue(info.getContactPerson1().toString());
+                user.child("contact_number_person1").setValue(info.getContactNumber1().toString());
+                user.child("contact_person2").setValue(info.getContactPerson2().toString());
+                user.child("contact_number_person2").setValue(info.getContactNumber2().toString());
+
+                if(info.getContactPerson3() != null && info.getContactNumber3()!= null) {
+                    user.child("contact_person3").setValue(info.getContactPerson3().toString());
+                    user.child("contact_number_person3").setValue(info.getContactNumber3().toString());
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(email,pass)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()){
+                                    Intent i = new Intent(getApplicationContext(), AccountCreated.class);
+                                    startActivity(i);
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "Registration failed. Please try again.", Toast.LENGTH_LONG).show();
+                                }
                             }
-                        }
-                    });
+                        });
 
+            }else{
+
+                confPassword.setError("Passwords do not match.");
+
+            }
         }else{
-
-            confPassword.setError("Passwords do not match.");
-
+            Toast.makeText(getApplicationContext(), "Password must be minimum of 6 letters with uppercase letters", Toast.LENGTH_LONG).show();
         }
     }
 
