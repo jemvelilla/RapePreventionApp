b32202b217365664d8696e827ece68b6f24c7470
jemvelilla <jemvelilla@gmail.com>
2018-01-22 23:36:59 +0800

    forgot password partial; auto-verify app through link

--- app/src/main/java/com/build1/rapepreventionapp/ForgotPassStep1.java
+++ app/src/main/java/com/build1/rapepreventionapp/ForgotPassStep1.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -14,28 +15,36 @@ import com.google.firebase.auth.ProviderQueryResult;
 
 public class ForgotPassStep1 extends AppCompatActivity {
 
-    FirebaseAuth auth;
+    private FirebaseAuth auth;
 
-    EditText emailAdd = (EditText) findViewById(R.id.editTextEmailAdd);
+    private EditText emailAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password_step1);
+
+        emailAdd = (EditText) findViewById(R.id.editTextEmailAdd);
+
+        auth = FirebaseAuth.getInstance();
     }
 
 
     public void btnOnClickSearchEmail (View view){
 
-        auth.fetchProvidersForEmail(emailAdd.getText().toString()).addOnCompleteListener(new OnCompleteListener<ProviderQueryResult>() {
+        String email = emailAdd.getText().toString().trim();
+        Log.v("email",email);
+
+        auth.fetchProvidersForEmail(email).addOnCompleteListener(new OnCompleteListener<ProviderQueryResult>() {
             @Override
             public void onComplete(@NonNull Task<ProviderQueryResult> task) {
 
                 boolean check = !task.getResult().getProviders().isEmpty();
 
-                if (!check){
+                if (check){
 
                     Intent i = new Intent(getApplicationContext(), ForgotPassStep2.class);
+                    i.putExtra("email", emailAdd.getText().toString());
                     startActivity(i);
 
                 } else {
--- app/src/main/java/com/build1/rapepreventionapp/ForgotPassStep2.java
+++ app/src/main/java/com/build1/rapepreventionapp/ForgotPassStep2.java
@@ -2,15 +2,40 @@ package com.build1.rapepreventionapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPassStep2 extends AppCompatActivity {
+
+    private String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password_step2);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        email = (String) getIntent().getSerializableExtra("email");
+        String emailAddress = email;
+
+        auth.sendPasswordResetEmail(emailAddress)
+            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(ForgotPassStep2.this, "Email sent.", Toast.LENGTH_LONG).show();
+                    } else{
+                        Toast.makeText(ForgotPassStep2.this, "Sending failed. Please try again.", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
     }
 
 
