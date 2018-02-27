143ce28dc807315a93ddebb893bdc7673bd06285
jemvelilla <jemvelilla@gmail.com>
2018-02-27 21:02:04 +0800

    Bluno Main - Push

--- app/src/main/java/com/build1/rapepreventionapp/Bluno/BlunoMain.java
+++ app/src/main/java/com/build1/rapepreventionapp/Bluno/BlunoMain.java
@@ -126,30 +126,30 @@ public class BlunoMain extends BlunoLibrary implements GoogleApiClient.Connectio
             public void onClick(View v) {
                 // TODO Auto-generated method stub
                 //setUpGClient();
-                final String message = EditInformation.firstName + " " + EditInformation.lastName + " needs help.";
-
-                    Map<String, Object> notificationMessage = new HashMap<>();
-                    notificationMessage.put("message", message);
-                    notificationMessage.put("from", mCurrentId);
-
-                    for (String id : ids) {
-                        mFirestore.collection("Users/" + id + "/Notifications").add(notificationMessage).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Toast.makeText(BlunoMain.this, "Notification sent.", Toast.LENGTH_SHORT).show();
-
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(BlunoMain.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-                    sendNotification();
+//                final String message = EditInformation.firstName + " " + EditInformation.lastName + " needs help.";
+//
+//                    Map<String, Object> notificationMessage = new HashMap<>();
+//                    notificationMessage.put("message", message);
+//                    notificationMessage.put("from", mCurrentId);
+//
+//                    for (String id : ids) {
+//                        mFirestore.collection("Users/" + id + "/Notifications").add(notificationMessage).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                            @Override
+//                            public void onSuccess(DocumentReference documentReference) {
+//                                Toast.makeText(BlunoMain.this, "Notification sent.", Toast.LENGTH_SHORT).show();
+//
+//                            }
+//                        }).addOnFailureListener(new OnFailureListener() {
+//                            @Override
+//                            public void onFailure(@NonNull Exception e) {
+//                                Toast.makeText(BlunoMain.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+//                            }
+//                        });
+//                    }
+                    //sendNotification();
 //                    getDeviceLocation();
 
-               // buttonScanOnClickProcess();										//Alert Dialog for selecting the BLE device                 //Alert Dialog for selecting the BLE device
+                buttonScanOnClickProcess();										//Alert Dialog for selecting the BLE device                 //Alert Dialog for selecting the BLE device
             }
         });
         Log.v("message", "onCreate");
